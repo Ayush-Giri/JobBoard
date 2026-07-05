7e85da4387b5060aa35cb7cc732b16f3415fcb3f
Ayush Giri <ayushgiri56@gmail.com>
2026-07-05 23:17:49 +0545

    modify skills models remove user field

--- skills/models.py
+++ skills/models.py
@@ -3,10 +3,9 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 
-User = get_user_model()
+
 class Skills(models.Model):
     name = models.CharField(max_length=200, unique=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} | {self.user.username}"
