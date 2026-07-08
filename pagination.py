23dd1622efcbdf0cf9145e6d495bb56354766b50
Ayush Giri <ayushgiri56@gmail.com>
2026-07-08 12:51:06 +0545

    make cursor pagination for industry

--- pagination.py
+++ pagination.py
@@ -1,7 +1,12 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, CursorPagination
 
 class BasicPagination(PageNumberPagination):
     page_size = 10
     max_page_size = 100
     page_query_param = "page"
     page_size_query_param = "page_size"
+
+
+class IndustryCursorPagination(CursorPagination):
+    page_size = 20
+    ordering = "-id"
