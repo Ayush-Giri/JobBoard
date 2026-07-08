db16fe17d0c72f66f2e349a5036227642c72289c
Ayush Giri <ayushgiri56@gmail.com>
2026-07-08 13:24:35 +0545

    add signal for automatic profile creation for employer

--- custom_user/signals.py
+++ custom_user/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from profile_management.models import ApplicantProfile
+from profile_management.models import ApplicantProfile, EmployerProfile
 
 User = get_user_model()
 
@@ -14,6 +14,7 @@ def manage_user_profile(sender, instance, created, **kwargs):
             ApplicantProfile.objects.create(
                 user=instance 
             )
+        EmployerProfile.objects.create(user=instance)
 
 
 
