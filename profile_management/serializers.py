cd4ee19f1bfed230af965add4351d6e33db921b2
Ayush Giri <ayushgiri56@gmail.com>
2026-07-12 13:00:44 +0545

    add applicant profile seriailizer

---
+++ profile_management/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from profile_management.models import ApplicantProfile
+from locations.serializers import CountrySerializer, CitySerializer
+
+
+class ApplicationProfileSerializer(serializers.ModelSerializer):
+    # front end will send ids to skill to add skills 
+    # and now with this skill names front end will get skill details as well
+    skill_names = serializers.StringRelatedField(source='skills', many=True, read_only=True)
+    city_detail = CitySerializer(source="city", read_only=True)
+    country_detail = CountrySerializer(source="country", read_only=True)
+    class Meta:
+        model = ApplicantProfile
+        fields = [
+            'id',
+            'date_of_birth',
+            'profile_photo',
+            'headline',
+            'bio',
+            'years_of_experience',
+            'current_job_title',
+            'current_company',
+            'expected_salary',
+            'country',
+            'country_detail',
+            'city',
+            'city_detail',
+            'skills',
+            'skill_names',
+            'linkedin_url',
+            'github_url',
+            'portfolio_url',
+            'resume'
+        ]
